6d9e70ab9514d51387a68ba8c0e3c52e1f16a1ca
emanuel <89155244+Emanueldiv1@users.noreply.github.com>
2024-08-25 16:01:07 -0300

    chatService: chave API

--- projectccmaster/src/main/java/com/tccmaster/projectccmaster/aplication/chatAI/service/ChatService.java
+++ projectccmaster/src/main/java/com/tccmaster/projectccmaster/aplication/chatAI/service/ChatService.java
@@ -1,5 +1,6 @@
 package com.tccmaster.projectccmaster.aplication.chatAI.service;
 
+import com.tccmaster.projectccmaster.aplication.chatAI.form.ChatForm;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,8 +15,10 @@ public class ChatService {
     @Autowired
     private RestTemplate restTemplate;
 
-    
-    public String generateContent(String requestBody) {
+    private final String API_KEY = "chave-da-api";
+    private final String URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + API_KEY;
+
+    public ChatForm generateContent(String requestBody) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
@@ -31,23 +34,33 @@ public class ChatService {
         if (response.getStatusCode() == HttpStatus.OK) {
             try {
                 JSONObject jsonResponse = new JSONObject(response.getBody());
-                JSONArray candidates = jsonResponse.getJSONArray("candidates");
-                if (candidates.length() > 0) {
-                    JSONObject candidate = candidates.getJSONObject(0);
+                JSONArray sugestoes = jsonResponse.getJSONArray("candidates");
+                if (sugestoes.length() > 0) {
+                    JSONObject candidate = sugestoes.getJSONObject(0);
                     JSONObject content = candidate.getJSONObject("content");
                     JSONArray parts = content.getJSONArray("parts");
                     if (parts.length() > 0) {
-                        return parts.getJSONObject(0).getString("text");
+                        ChatForm message = new ChatForm();
+                        String rawText = parts.getJSONObject(0).getString("text");
+
+                        String formattedText = rawText
+                                .replace("#", "")
+                                .replace("*", "")
+                                .replaceAll("\\\\n", "\n")
+                                .replaceAll("\\\\t", "\t");
+
+                        message.setMessage(formattedText);
+                        return message;
                     }
                 }
             } catch (JSONException e) {
-                throw new RuntimeException("Failed to parse response: " + e.getMessage());
+                throw new RuntimeException("Falha ao analisar a resposta: " + e.getMessage());
             }
         } else {
-            throw new RuntimeException("Failed to call API: " + response.getStatusCode());
+            throw new RuntimeException("Falha ao chamar a API:" + response.getStatusCode());
         }
 
         return null;
     }
-
 }
+
